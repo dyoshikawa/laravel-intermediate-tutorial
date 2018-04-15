2a368181420f4b4a5e5fc6c5ab5472c84333fffa
dyoshikawa <dyoshikawa1993@gmail.com>
2018-04-15 20:59:08 +0900

    fix first-boot.sh laravelバージョン指定

--- first-boot.sh
+++ first-boot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v `pwd`/:/app dyoshikawa/laravel:dev bash -c "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
+docker run --rm -v `pwd`/:/app dyoshikawa/laravel:5.6 bash -c "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
